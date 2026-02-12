22503b561bc2d278c42de7be7676a5e178fd458c
paulbgtr <pbg.work@pm.me>
2026-02-12 16:32:27 +0200

    change flow
    selected work, services, process, about.

--- app/page.tsx
+++ app/page.tsx
@@ -21,13 +21,13 @@ export default function Page() {
 
         <Separator />
 
-        <About />
+        <SelectedWork />
 
         <Services />
 
         <Process />
 
-        <SelectedWork />
+        <About />
 
         <Separator />
 
